983150ad60938c014bb3eb19e8bca79629bb2fc5
ryanxu <peaceful321@163.com>
2017-10-10 19:16:15 +0800

    python learn

---
+++ basic/StringDemo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/9 上午11:46
+# @Author  : Ryan Xu
+# @Site    : 
+# @File    : StringDemo.py
+# @Software: PyCharm
+
+print("包含中文的 str")
+
+
+#获取字符的整数表示
+print(ord("中"))
+
+
+#chr 把编码转换为对应的字符
+print(chr(20013))
+
+n1 = 255
+print(hex(n1))
